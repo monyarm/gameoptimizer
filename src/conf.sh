9072ca9c78f8b4a32dc3257cf0e7a9486f13ff07
monyarm <monyarm@gmail.com>
2022-06-25 19:42:25 +0300

    fixes to deflopt in pngs, size printing, and path resolution

--- src/conf.sh
+++ src/conf.sh
@@ -43,7 +43,7 @@ upx_conf=( "$IN/**/*.x86" "$IN/**/*.x86_64" "$IN/**/*.exe" "$IN/**/*.dll" "$IN/*
     "$IN/**/bin_unix/linux_64_server" "$IN/**/bin_unix/linux_client"\
     "$IN/**/bin_unix/linux_server" "$IN/**/doomrl" "$IN/**/libs/Dwarf_Fortress"\
     "$IN/**/Funkin" "$IN/**/EDOPro" "$IN/**/freeorion/freeorion" "$IN/**/freeoriond"\
-"$IN/**/freeorionca" "$IN/**/Unciv" "$IN/**/Mindustry" "$IN/**/minecraft-launcher" )
+"$IN/**/freeorionca" "$IN/**/Unciv" "$IN/**/Mindustry" "$IN/**/minecraft-launcher" "$IN/**/minecraft-launcher" "$IN/**/endless-sky" )
 
 archive_conf=( "$IN/**/*.zip" "$IN/**/*.apk" "$IN/**/*.jar" "$IN/**/*.rpa" "$IN/**/*.pk3"\
     "$IN/**/*.pkz" "$IN/**/*.pke" "$IN/**/*.ipk3" "$IN/**/*.pk7" "$IN/**/*.ipk7"  "$IN/**/*.epk" \
--- src/optim.sh
+++ src/optim.sh
@@ -15,7 +15,16 @@ case $1 in
     ;;
 esac
 
-REALPATH="$( realpath ${BASH_SOURCE%/*})/src"
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+REALPATH="$( realpath $DIR)"
 DEPS="$REALPATH/deps/"
 NODE_MODULES="$DEPS/node_modules/"
 
